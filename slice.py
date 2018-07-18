19d0e8df06e00dbd4c96ae21b52e0f119ddcc590
Scott Long <neuralnorthwestllc@gmail.com>
2018-07-18 02:42:04 +0000

    smaller buffer

--- slice.py
+++ slice.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 
 import sys
+import io
 
 # Slice each file into 32 parts by dealing
 NUM_SLICES = 32
 numlines = 0
 for f in sys.argv[1:]:
-	outf = [open('slices/%s-slice%02d' % (f, i), 'w') for i in xrange(NUM_SLICES)]
-	inf = open(f, 'r')
+	outf = [io.open('slices/%s-slice%02d' % (f, i), 'w', 1024*1024/NUM_SLICES) for i in xrange(NUM_SLICES)]
+	inf = io.open(f, 'r', 1024*1024)
 	i = 0
 	for line in inf:
 		numlines += 1
